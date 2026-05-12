4e5f9a31900d8c14bdc44f81d659438f9cbf9ec5
indykite <66900860+indykiteone@users.noreply.github.com>
2026-05-12 14:58:20 -0600

    ci(sync): update GitHub Actions (#62)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .github/actions/commitlint/.commitlintrc.mjs
+++ .github/actions/commitlint/.commitlintrc.mjs
@@ -2,24 +2,10 @@
 // DO NOT EDIT!!!
 // Managed by GitHub Actions
 //
-const actor = process.env.GITHUB_ACTOR || "";
-const releaseBotName = process.env.RELEASE_BOT || "";
-function isBotCommit(message) {
-  return (
-    message.includes("Signed-off-by: dependabot[bot]") ||
-    (message.startsWith("ci(sync): update GitHub Actions") && actor === "renovate[bot]") ||
-    (message.startsWith("ci(deps): update") && actor === "renovate[bot]") ||
-    (message.startsWith("test(deps): update") && actor === "renovate[bot]") ||
-    (message.startsWith("chore(deps): update") && actor === "renovate[bot]") ||
-    (message.startsWith("chore(deps): Pin dependencies") && actor === "renovate[bot]") ||
-    (message.startsWith("chore(master): release") && actor == releaseBotName)
-  );
-}
 export default {
   extends: ["@commitlint/config-conventional"],
   defaultIgnores: false,
   failOnWarnings: true,
-  ignores: [(message) => isBotCommit(message)],
   rules: {
     "body-max-line-length": [2, "always", 72],
     "header-max-length": [2, "always", 72],
@@ -68,23 +54,7 @@ export default {
           }
           return [true];
         },
-        "contains-jira-ticket": ({ header, body, footer }) => {
-          if (isBotCommit(header)) {
-            return [true];
-          }
-          let regex = /\bENG\-[0-9]{2,}\b/;
-          if (regex.test(header)) {
-            return [false, "Please, move your JIRA ticket into commit body, and do not include in header directly."];
-          }
-          if (regex.test(body) === false && regex.test(footer) === false) {
-            return [
-              false,
-              "Your commit message is missing JIRA ticket. Consider adding it to commit body or ignore this error.",
-            ];
-          }
-          return [true];
         },
-      },
     },
   ],
 };
